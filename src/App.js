dbc231cae4f8b223f297b975fd225017aeb30a08
Steven <julianospina250@gmmail.com>
2024-07-26 11:12:22 -1200

    se agrega lo nuevo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './paginas/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Login />}/>
+        </Routes>
+      </Router>
+    </Fragment>
+  );  
 }
 
 export default App;
